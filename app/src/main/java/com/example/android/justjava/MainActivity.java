fc8437a23ee7bd52f4d728dc0f47a83a64f70044
JulianMiller <julianmiller@me.com>
2018-02-11 17:54:37 -0600

    final logic for name, chocolate, and whipped cream

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,11 +9,12 @@
 package com.example.android.justjava;
 
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -33,21 +34,35 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText nameField = (EditText)  findViewById(R.id.name_field);
+        String name = nameField.getText().toString();
+        Log.v("MainActivity","Name" + name);
+
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
+
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
         displayMessage(priceMessage);
 
     }
 
-    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate ) {
-        String priceMessage = "Name: King Julian";
+    /**
+     *
+     * @param name
+     * @param price
+     * @param addWhippedCream
+     * @param addChocolate
+     * @return
+     */
+    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate ) {
+        String priceMessage = "Name: " + name;
         priceMessage += "\nAdd whipped cream? " + addWhippedCream;
         priceMessage += "\nAdd chocolate? " + addChocolate;
-        priceMessage += "\nQuantity" + quantity;
+        priceMessage += "\nQuantity " + quantity;
         priceMessage += "\nTotal: $" + price;
         priceMessage += "\nThank you!";
         return priceMessage;
@@ -72,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         quantity = quantity-1;
         display(quantity);
     }
+
 
     /**
      * This method displays the given quantity value on the screen.

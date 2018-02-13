bdde8855512f0f9e8b92f1f53e99d79ce88c3b1a
JulianMiller <julianmiller@me.com>
2018-02-13 10:49:48 -0600

    moved messages to email intent

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,6 +9,8 @@
 package com.example.android.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,6 +18,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -44,11 +47,20 @@ public class MainActivity extends AppCompatActivity {
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
-        int price = calculatePrice();
+        int price = calculatePrice(hasWhippedCream,hasChocolate);
         String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
-        displayMessage(priceMessage);
+
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 
     }
+
 
     /**
      *
@@ -69,12 +81,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+    private int calculatePrice(boolean addWhippedCream,boolean addChocolate) {
         int basePrice = 5;
 
         if (addWhippedCream) {
             basePrice = basePrice + 1;
         }
+
         if (addChocolate){
             basePrice = basePrice + 2;
         }
@@ -84,6 +97,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, "100 coffees is our limit.", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity +1;
         display(quantity);
     }
@@ -91,8 +108,13 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
-        quantity = quantity-1;
+        if (quantity == 0) {
+            Toast.makeText(this, "Please add at least 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        quantity = quantity - 1;
         display(quantity);
+
     }
 
 
@@ -104,12 +126,4 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
 }
